a2f0b0dc4600ab45f0e2fd2f2a816b8068243ffc
Jeff McGovern <jeffreydmcgovern@gmail.com>
2023-03-27 01:41:19 -0700

    initial implementation of a fibonacci value printer

---
+++ fibonacci.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import sys
+
+import argparse
+
+_DESCRIPTION='A tool to print the Fibonacci Sequence or evaluate a value at a specific index.'
+def get_arg_parser():
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description=_DESCRIPTION)
+    parser.add_argument('index', type=int, help='the term number or index to print the value of')
+    return parser
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def main():
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    print(fibonacci(args.index))
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
