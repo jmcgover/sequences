989eb8cf68656e894bdd2c1865543fc5e4405aef
Jeff McGovern <jeffreydmcgovern@gmail.com>
2023-03-27 01:48:01 -0700

    add some type hinting

--- fibonacci.py
+++ fibonacci.py
@@ -5,19 +5,19 @@ import sys
 import argparse
 
 _DESCRIPTION='A tool to print the Fibonacci Sequence or evaluate a value at a specific index.'
-def get_arg_parser():
+def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog=sys.argv[0], description=_DESCRIPTION)
     parser.add_argument('index', type=int, help='the term number or index to print the value of')
     return parser
 
-def fibonacci(n):
+def fibonacci(n: int) -> int:
     if n == 0:
         return 0
     if n == 1:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-def main():
+def main() -> int:
     parser = get_arg_parser()
     args = parser.parse_args()
     print(fibonacci(args.index))
