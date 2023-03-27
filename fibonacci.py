7d9f27191d733d6eaef9e5d5f10209fe9105cb50
Jeff McGovern <jeffreydmcgovern@gmail.com>
2023-03-27 03:25:44 -0700

    add a lister of the fibonacci numbers

--- fibonacci.py
+++ fibonacci.py
@@ -6,7 +6,6 @@ import functools
 import typing
 import sys
 
-
 _DESCRIPTION='A tool to print the Fibonacci Sequence or evaluate a value at a specific index.'
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog = sys.argv[0], description = _DESCRIPTION)
@@ -21,13 +20,21 @@ def fibonacci(n: int) -> int:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+def list_fibonacci(n:int) -> list[int]:
+    fib_list = []
+    for i in range(n + 1):
+        fib_list.append(fibonacci(i))
+    return fib_list
+
 def main() -> int:
     parser = get_arg_parser()
     args = parser.parse_args()
+    n = args.index
+
     if args.print:
-        print('lol')
+        print(list_fibonacci(n))
     else:
-        print(fibonacci(args.index))
+        print(fibonacci(n))
     return 0
 
 if __name__ == '__main__':
