77660cfabdcd47e4cf06d8ab2decadf55981a2d3
Jeff McGovern <jeffreydmcgovern@gmail.com>
2023-03-27 02:16:03 -0700

    a bad memoized implementation that's slower than the original

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
-import sys
+import functools
+import typing
 
+import sys
 import argparse
 
 _DESCRIPTION='A tool to print the Fibonacci Sequence or evaluate a value at a specific index.'
@@ -9,6 +11,24 @@ def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog=sys.argv[0], description=_DESCRIPTION)
     parser.add_argument('index', type=int, help='the term number or index to print the value of')
     return parser
+
+class Memoized():
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, typing.Hashable):
+            # Some objects, e.g. immutable ones like lists, are not hashable, so
+            # we just return the executed function value.
+            return self.func(*args)
+        value = self.func(*args)
+        self.cache[args] = value
+        return value
+    def __repr_(self):
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        return functools.partial(self.__call__, obj)
+
 
 def fibonacci(n: int) -> int:
     if n == 0:
